f1ffa6fddbef5dd8b693dc72a1e6c4790bb3daa9
hanxixuana <xixuanhan@gmail.com>
2018-05-17 20:53:14 +0800

    good status

--- examples/property_inspection_prediction/run.sh
+++ examples/property_inspection_prediction/run.sh
@@ -2,11 +2,11 @@
 
 echo "Training GBM..."
 ./lightgbm config_file=gbtrain.conf > gblog.txt
-./draw.py dblog.txt
+./draw.py gblog.txt
 
 echo "Training DBM..."
 ./lightgbm config_file=dbtrain.conf > dblog.txt
-./draw.py gblog.txt
+./draw.py dblog.txt
 
 display db_pic.png &
 display gb_pic.png &
